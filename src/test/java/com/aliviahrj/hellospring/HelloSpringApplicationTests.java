03881c6c3cbcf5222266107b4b7c2c2d8dcca9ec
Aliviah <aliviah.rodejones@gmail.com>
2026-04-28 20:33:15 -0500

    Hello Spring boot app

---
+++ src/test/java/com/aliviahrj/hellospring/HelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aliviahrj.hellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
