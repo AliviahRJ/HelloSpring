3504bf87836b07c8573071625cba9be1ff4f72dc
Aliviah <aliviah.rodejones@gmail.com>
2026-04-28 20:33:20 -0500

    Hello Spring boot app

---
+++ src/main/java/com/aliviahrj/hellospring/HelloController.java
@@ -0,0 +1,22 @@
+package com.aliviahrj.hellospring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String greeting(Model model){
+
+        String name = "Aliviah";
+        model.addAttribute("name", name);
+        return "hello";
+    }
+
+    @GetMapping("/about")
+    public String about(){
+        return "about";
+    }
+}
